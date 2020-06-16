74cc10983c2870492aedbc42266b1efcf8f0e030
pipoperations <pipoperations@pipusa.com>
2020-06-16 14:00:18 -0400

    Initial on server

--- processedi.tcl
+++ processedi.tcl
@@ -17,6 +17,11 @@ set ProcessedPath "/home/eclipseftp/processed/"
 ## Host       10.10.10.10
 ## Username         Brianisawesome
 ## Password         W3lc0m3!
+<<<<<<< HEAD
+=======
+## PushDirectory    ftp-in
+## PullDirectory    ftp-out
+>>>>>>> Initial on server
 ##  Use tabs between keys and values
 
 set systemTime [clock seconds]
@@ -35,12 +40,25 @@ proc ListFiles {filepath} {
 
 # Proceedure to parse customer data files into a key value list
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> Initial on server
 proc getCustomerData {filename} {
     set openFile [open $filename r]
     set data [read -nonewline $openFile]
     close $openFile
+<<<<<<< HEAD
     set dataList [split $data "\n"]
+=======
+
+    ## split the file into lines
+
+    set dataList [split $data "\n"]
+
+    ## parse each line for key/value pairs with tab "\t" as the delimiter
+
+>>>>>>> Initial on server
     foreach dataLine $dataList {
         lappend CustomerData [string trim [string range $dataLine 0 [string first "\t" $dataLine]]]
         lappend CustomerData [string trim [string range $dataLine [string first "\t" $dataLine] [string length $dataLine]]]
@@ -60,6 +78,10 @@ proc CustomerList {filelist} {
 }
 
 # Parses data files and extracts the customer ID an matches with a connection string
+<<<<<<< HEAD
+=======
+
+>>>>>>> Initial on server
 proc ParseFile {filename ConfigPath} {
     # search file for unique key
     set dataFile [open $filename r]
@@ -78,6 +100,7 @@ proc ParseFile {filename ConfigPath} {
 }
 
 proc SendFile {file connectionstring} {
+<<<<<<< HEAD
     set username [dict get $connectionstring Username]
     set password [dict get $connectionstring Password]
     set ipAddress [dict get $connectionstring Host]
@@ -97,6 +120,62 @@ proc SendFile {file connectionstring} {
     expect "> " { send "quit" }
 }
 
+=======
+
+    ## populate our working variables from the customer dictionary of key value pairs
+
+    set username [dict get $connectionstring Username]
+    set password [dict get $connectionstring Password]
+    set ipAddress [dict get $connectionstring Host]
+    set protocol [dict get $connectionstring Protocol]
+    set customerName [dict get $connectionstring CustomerName]
+
+    ## make sure that a value exists for the PushDirectory
+
+    if {[dict exists $connectionstring PushDirectory]} {
+        set pushDirectory [dict get $connectionstring PushDirectory]
+    } else {
+        set pushDirectory ""
+    }
+
+    ## make sure that a value exists for the PullDirectory
+
+    if {[dict exists $connectionstring PullDirectory]} {
+        set pullDirectory [dict get $connectionstring PullDirectory]
+    } else {
+        set pushDirectory ""
+    }
+    puts "$protocol"
+    switch $protocol {
+        sftp {
+            puts "$username $password $ipAddress"
+            spawn sftp "$username@$ipAddress"
+            expect {
+                "assword" {
+                    send "$password\r"
+                }
+                "yes/no" {
+                    send "yes\r"
+                }
+                "Permission"{
+                    return -code ok
+                }
+            }
+            expect "> " {send "cd $pushDirectory\r"}
+            expect "> " { send "put $file\r" }
+            expect "> " { send "quit" }
+        }
+        smb {
+            puts "$connectionstring"
+            return -code ok
+        }
+        default {
+            puts "Invalid protocol"
+            return -code error \ "protocol not set or invalid."
+        }
+    }
+}
+>>>>>>> Initial on server
 proc MoveFile {filename} {
     # move file after success
     upvar 2 ProcessedPath path
@@ -143,8 +222,18 @@ proc ProcessCustomer {path configpath} {
     foreach file $dataFileList {
         set hasCustomer [ParseFile $file $configpath]
         if { $hasCustomer > 0 } {
+<<<<<<< HEAD
            SendFile $file $hasCustomer
            MoveFile $file
+=======
+            puts [SendFile $file $hasCustomer]
+##          if {[SendFile $file $hasCustomer] == 0} {
+##
+##                ## SendFile should return 0 if it is successful 
+##                
+##                MoveFile $file
+##           }
+>>>>>>> Initial on server
         } else {
             puts "$file Doesn't have a customer $hasCustomer"
         }
@@ -153,6 +242,10 @@ proc ProcessCustomer {path configpath} {
 }
 
 # Main output
+<<<<<<< HEAD
+=======
+
+>>>>>>> Initial on server
 puts [ProcessCustomer $GlobalPathin $ConfigPath]
 
 # expect -timeout -1 eof
